f1f1ef72721a516694e7ca1c622dc97b138cfb4c
Dominik Ferber <dominik.ferber@gmail.com>
2015-07-18 13:30:36 +0200

    better error handling on server

--- app/server/server.js
+++ app/server/server.js
@@ -7,9 +7,16 @@ app.use(express.static('public'))
 app.get('*', (req, res) => {
   generatePage(req.path, req.query).then(
     html => res.end(html),
-    error => res.end(error ? error.toString() : 'Could not generate page')
+    error => res.end(error ? printError(error) : 'Could not generate page')
   )
 })
 
 app.listen(3000)
 if (__DEV__) console.log('server listening')
+
+function printError (error) {
+  return `
+    <p><b>${error.toString()}</b></p>
+    <code>${error.stack}</code>
+  `
+}
