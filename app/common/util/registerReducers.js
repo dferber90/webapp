86b91cf5c2095b9cabd8ea698038ecaf687de828
Dominik Ferber <dominik.ferber@gmail.com>
2015-07-21 11:38:26 +0200

    add getDisplayName source comment

--- app/common/util/registerReducers.js
+++ app/common/util/registerReducers.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+// duplicated from
+// github.com/gaearon/react-redux/blob/master/src/utils/getDisplayName.js
 function getDisplayName (component) {
   return component.displayName || component.name || 'Component'
 }
