63340c62e30b641a5893a2ba8ed5e7e2514ca5ee
Darío Javier Cravero <dario@uxtemple.com>
2015-07-23 14:38:53 +0100

    Namespace variables exposed to the browser
    If you put all variables in directly on `window` you may find some odd issues, when something, generally a react component, needs, say, `React` present but the dev forgot to include them. Linting should warn about this but it's a good thing to prevent devs from those potential side-effects later on. Thoughts?

--- app/client/client.js
+++ app/client/client.js
@@ -70,10 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // expose variables to browser
   if (__DEV__) {
-    window.store = store
-    window.React = React
-    window.ReactDOM = ReactDOM
-    window.Router = Router
-    window.history = history
+    window.playground = { history, store, React, ReactDOM, Router };
   }
 })
