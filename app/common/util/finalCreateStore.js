8cc8adbe9dbc2bfebcb211a2f7756c5c78f82ca2
Dominik Ferber <dominik.ferber@gmail.com>
2015-08-22 09:22:11 +0200

    add redux-logger

--- app/common/util/finalCreateStore.js
+++ app/common/util/finalCreateStore.js
@@ -1,14 +1,32 @@
-import { compose, createStore } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
 
 // TODO add async middleware (aka promiseMiddleware via applyMiddleware())
 // https://github.com/gaearon/redux/blob/improve-docs/docs/middleware.md
 let finalCreateStore
-if (__DEV__ && __DEVTOOLS__) {
-  const { devTools } = require('redux-devtools')
-  finalCreateStore = compose(
-    devTools(),
-    createStore
-  )
+if (__DEV__) {
+
+  let composedCreateStore
+  if (__DEVTOOLS__) {
+    const { devTools } = require('redux-devtools')
+    composedCreateStore = compose(
+      devTools(),
+      createStore
+    )
+  } else {
+    composedCreateStore = createStore
+  }
+
+
+  if (__CLIENT__) {
+    const createLogger = require('redux-logger')
+    const logger = createLogger()
+    const createStoreWithMiddleWare = applyMiddleware(
+      logger
+    )(composedCreateStore)
+    finalCreateStore = createStoreWithMiddleWare
+  } else {
+    finalCreateStore = composedCreateStore
+  }
 } else {
   finalCreateStore = createStore
 }
